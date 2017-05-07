e8907b6dff710a93f4ac056cc5d19e4e28fe1803
Monkeyninja55 <kap666@yahoo.com>
2017-05-06 22:16:55 -0400

    added session vars for supplies

--- mainPages/supplies/supplies.php
+++ mainPages/supplies/supplies.php
@@ -12,19 +12,47 @@
 		</h2>
 	</div>
 	
-	<div class="options">
-		<p class="optionsList">
-	    oxen<br>
-	    sets of clothing<br>
-	    bullets<br>
-	    wagon wheels<br>
-	    wagon axles<br>
-	    wagon tongues<br>
-	    pounds of food<br>
-	    money left<br><br><br>
+	<div id="mainOptions">
+			<table id="storeTable">
+				<tr>
+					<th>1. Oxen</th>
+					<th><?php echo $_SESSION['oxen'];?></th>
+				</tr>
+				<tr>
+					<th>2. Food</th>
+					<th><?php echo $_SESSION['food'];?></th>
+				</tr>
+				<tr>
+					<th>3. Clothing</th>
+					<th><?php echo $_SESSION['clothing'];?></th>
+				</tr>
+				<tr>
+					<th>4. Ammunition</th>
+					<th><?php echo $_SESSION['ammo'];?></th>
+				</tr>
+				<tr>
+					<th>5. Wagon Wheels</th>
+					<th><?php echo $_SESSION['wheels'];?></th>
+				</tr>
+				<tr>
+					<th>6. Wagon axles</th>
+					<th><?php echo $_SESSION['axles'];?></th>
+				</tr>
+				<tr>
+					<th>7. Wagon tongues</th>
+					<th><?php echo $_SESSION['tongues'];?></th>
+				</tr>
+				<tr>
+					<th>Amount you have: &emsp;&emsp;&emsp;</th>
+					<th><?php echo $_SESSION['money'];?></th>
+				</tr>
+			</table>
+
+		<br>
+		<p class="spaceToContinue textAlignCenter">
+			Press SPACE BAR to continue 
 		</p>
     </div>
-	<div class="title">Press SPACE BAR to continue</div>
 
 	<script>
 		document.body.onkeyup = function(e){
