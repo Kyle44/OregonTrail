546c1ab126ac438f4a3f0e7640c102f31a9bfb58
Kyle <fritzk1@umbc.edu>
2017-05-13 18:32:45 -0400

    Fixed title for main.html

--- mainPages/main.js
+++ mainPages/main.js
@@ -1,13 +1,62 @@
-
 function directInput() {
 	document.getElementById("optionsChoice").focus();
 }
 
 $(document).ready(function(){
 
+	function getMonth(monthNum) {
+		switch(monthNum) {
+			case 1:
+				return "January";
+			case 2:
+				return "February";
+			case 3:
+				return "March";
+			case 4:
+				return "April";
+			case 5:
+				return "May";
+			case 6:
+				return "June";
+			case 7:
+				return "July";
+			case 8:
+				return "August";
+			case 9:
+				return "September";
+			case 10:
+				return "October";
+			case 11:
+				return "November";
+			case 12:
+				return "December";
+		} // end switch
+	} // end function
+
+
 	var game = JSON.parse(window.sessionStorage.game);
 
+	if(!game.weather){
+		game.weather = 'cool';
+	}
+	if(!game.health){
+		game.health = 'good';
+	}
+	if(!game.pace){
+		game.pace = 'steady';
+	}
+	if(!game.rations){
+		game.rations = 'filling';
+	}
+	if(!game.location){
+		game.location = 'Independence';
+	}
+	window.sessionStorage.game = JSON.stringify(game);
+
 	var inTown = true; // must be calculated later
+
+	$('#dateLocation').html(game.location + "<br>" + getMonth(game.month) + " " + game.day + ", " + game.year);
+
 
 	$("#weather").text(game.weather);
 	$("#health").text(game.health);
@@ -17,48 +66,47 @@ $(document).ready(function(){
 	// get option set
 	$("#optionsChoice").keydown(function(e) {
 		if (e.keyCode == 13) {
-		var choice = parseInt($(this).val());
+			var choice = parseInt($(this).val());
 
-		if(!isNaN(choice) && choice <= 9 && choice >= 1){
-			switch(choice) {
-				case 1:
-					break;
-				case 2:
-					location.replace("supplies/supplies.html");
-					break;
-				case 3:
-					location.replace("map/map.html");
-					break;
-				case 4:
-					location.replace("pace/pace.html");
-					break;
-				case 5:
-					location.replace("rations/rations.html");
-					break;
-				case 6:
-					// location.replace("rest/rest.php");
-					break;
-				case 7:
-					location.replace("trade/trade.php");
-					break;
-				case 8:
-					if (inTown) {
-						// location.replace("talk/talk.php");
-					}
-					else {
-						// ;location.replace("fishing/fishing.html");
-					}
-					break;
-				case 9:
-					if (inTown) {
-						location.replace("buy/buy.html");
-					}
-					break;
-				default:
-					;
-			} // end switch
-		} // end if
->>>>>>> 8e8e5ee41aac294774b7ae1f1ad99f71464eb5bf
-
+			if(!isNaN(choice) && choice <= 9 && choice >= 1){
+				switch(choice) {
+					case 1:
+						break;
+					case 2:
+						location.replace("supplies/supplies.html");
+						break;
+					case 3:
+						location.replace("map/map.html");
+						break;
+					case 4:
+						location.replace("pace/pace.html");
+						break;
+					case 5:
+						location.replace("rations/rations.html");
+						break;
+					case 6:
+						// location.replace("rest/rest.php");
+						break;
+					case 7:
+						location.replace("trade/trade.php");
+						break;
+					case 8:
+						if (inTown) {
+							// location.replace("talk/talk.php");
+						}
+						else {
+							// ;location.replace("fishing/fishing.html");
+						}
+						break;
+					case 9:
+						if (inTown) {
+							location.replace("buy/buy.html");
+						}
+						break;
+					default:
+						;
+				} // end switch
+			} // end if
+		}
 	}); // end keydown
 });
