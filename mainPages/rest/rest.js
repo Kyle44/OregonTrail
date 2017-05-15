ef502834843bcdffb7a4a8ee6c15c22eea5c71b4
Monkeyninja55 <kap666@yahoo.com>
2017-05-14 21:08:42 -0400

    Update rest.js

--- mainPages/rest/rest.js
+++ mainPages/rest/rest.js
@@ -10,6 +10,12 @@ $(document).ready(function(){
             var choice = parseInt($(this).val());
             if(!isNaN(choice) && choice <= 9 && choice >= 0){
 		calcDays(choice, game);
+		if (game.health < 9) {
+			game.health += choice;
+			if (game.health > 9) {
+				game.health = 9;
+			}
+		}
                 window.sessionStorage.game = JSON.stringify(game);
                 location.replace("../main.html");
             }
